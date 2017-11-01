2d0212d2cbab4f1dc80367aa5ea6df24174f7361
lucasgsfelix <lucasgsfelix@gmail.com>
2017-11-01 21:42:46 -0200

    Add files via upload

---
+++ mundoWumpus.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2.7.12
+#-*- coding: utf-8 -*-
+from random import randint
+from mapa import *
+
+
+if __name__ == "__main__":
+
+	mapa = [[0,0,0,0],
+			[0,0,0,0],
+			[0,0,0,0],
+			[0,0,0,0]]
+
+	mapaJogador =  [[0,0,0,0],
+					[0,0,0,0],
+					[0,0,0,0],
+					[0,0,0,0]]
+
+	mapa =  definePocoWumpus(mapa)  #### ao fim mapa está pronto
+
+
+
